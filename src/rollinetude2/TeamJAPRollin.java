eadc00cdca9bbaa33fed2b24fbffc7c714c7c1d2
prabhatgokul <prabhatgokul@gmail.com>
2016-03-08 14:58:58 +1300

    start two of a kind method

--- src/rollinetude2/TeamJAPRollin.java
+++ src/rollinetude2/TeamJAPRollin.java
@@ -5,6 +5,8 @@
  */
 package rollinetude2;
 
+import java.util.HashMap;
+
 /**
  *
  * @author James
@@ -25,14 +27,14 @@ public class TeamJAPRollin extends Rollin {
         {{0, 3, 5}, {1, 2, 4}},
         {{0, 4, 5}, {1, 2, 3}}
     };
-    
+
     public TeamJAPRollin(int[] dice) {
         super(dice);
     }
 
-     /**
+    /**
      * Determine whether the dice at a given triple of indices form a set.
-     * 
+     *
      * @param indices the indices
      * @return true if the dice at those indices form a set, false otherwise.
      */
@@ -49,46 +51,48 @@ public class TeamJAPRollin extends Rollin {
         if (a == b || a == c || b == c) {
             return false;
         }
-        
+
         // If all three are different and largest minus smallest is 2 then it
         // is a set, otherwise not.
         int max = Math.max(a, Math.max(b, c));
         int min = Math.min(a, Math.min(b, c));
         return max - min == 2;
     }
-    
+
     /**
      * Make change in dice in method
+     *
      * @param roll
-     * @return 
+     * @return
      */
     @Override
     public int handleRoll(int roll) {
         // declare variables
         boolean[][] completeRow = new boolean[9][2];
-        int [] incompleteSet = new int[3];
-        int [] completeSet = new int[3];
+        int[] incompleteSet = new int[3];
+        int[] completeSet = new int[3];
         int row = 0;
         int incompleteCol = 0;
-        int [] count = new int[7];
+        int[] count = new int[7];
 
         // find which rows have sets
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 2; j++) { 
+            for (int j = 0; j < 2; j++) {
                 completeRow[i][j] = isSet(setIndices[i][j]);
                 System.out.print(completeRow[i][j]);
             }
             System.out.println();
         }
-        
+
         // store last row with set in row
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 2; j++) {
                 if (completeRow[i][j]) {
-                    row = i;                }
+                    row = i;
+                }
             }
         }
-        
+
         // determine which column has incomplete row
         // initialise incomplete and complete set indices
         if (completeRow[row][0]) {
@@ -100,7 +104,7 @@ public class TeamJAPRollin extends Rollin {
             completeSet = setIndices[row][1];
             incompleteSet = setIndices[row][0];
         }
-        
+
         // test print statements
         System.out.println("row: " + row + ", Incomplete col: " + incompleteCol);
         System.out.print("Complete set: ");
@@ -108,45 +112,60 @@ public class TeamJAPRollin extends Rollin {
             System.out.print(i + " ");
         }
         System.out.print("Incomplete set: ");
-        
+
         for (int i : incompleteSet) {
             System.out.print(i + " ");
         }
         System.out.println();
-        
+
         // initialise count array
-        for(int i = 0; i < incompleteSet.length; i++){
+        for (int i = 0; i < incompleteSet.length; i++) {
             count[dice[incompleteSet[i]]]++;
         }
-        
+
         // test print statements - count array
         System.out.print("count array: ");
         for (int i = 0; i < count.length; i++) {
             System.out.println("dice: " + i + " count: " + count[i]);
         }
         System.out.println();
+        
+//        for (int i = 0; i < 3; i++) {
+//            if (count[])
+//        } 
+            
+        if (count[1] == 1 || count[6] == 1) {
+            return closeEndedCase(roll, incompleteSet, count);
+        }
 
         return 0;
     }
-    
-    private int closeEndedCase(int roll, int[] incompleteSet, int[] count) {
-        int replaceIndex = 0;
-        int[] replaceArray = new int[3];
-        if (count[1] == 1) {
-            
-            for (int i = 0; i < 3; i++) {
-                replaceArray[i] =
-            }
-            replaceIndex = 0;
-            replaceArray[0] = roll;
-            replaceArray[1] = dice[incompleteSet[1]];
-            replaceArray[2] = dice[incompleteSet[2]];
-            if (isSet(replaceArray)) {
-                dice[incompleteSet[replaceIndex]] = roll;
-                return incompleteSet[replaceIndex];
-            }
-            
-        }
-    }
 
+    private int closeEndedCase(int roll, int[] incompleteSet, int[] count) {
+        int[] tempArray = new int[3];
+             for (int i = 0; i < 3; i++) {
+                tempArray[0] = dice[incompleteSet[0]];
+                tempArray[1] = dice[incompleteSet[1]];
+                tempArray[2] = dice[incompleteSet[2]];
+                tempArray[i] = roll;
+                if (isSet(tempArray)) {
+                    dice[incompleteSet[i]] = roll;
+                    return incompleteSet[i];
+                }
+            }
+            for (int i = 0; i < 3; i++) {
+                if (dice[incompleteSet[i]] == 1) {
+                    dice[incompleteSet[i]] = roll;
+                    return incompleteSet[i];
+                } else if (dice[incompleteSet[i]] == 6) {
+                    dice[incompleteSet[i]] = roll;
+                    return incompleteSet[i];
+                }
+            }     
+        return -1;
+    }
+    
+    private int twoOfAKind(int roll, int[]inCompleteSet, int[] count) {
+        return -1;
+    }
 }
